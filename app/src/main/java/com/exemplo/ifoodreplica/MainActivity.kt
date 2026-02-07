0fc7120cc17c8b02042340ddabb49334e9455a3e
dom_Ryan <ryanrodriguues06@gmail.com>
2026-02-07 16:38:41 -0300

    fix: fixing the bug on navigation menu

--- app/src/main/java/com/exemplo/ifoodreplica/MainActivity.kt
+++ app/src/main/java/com/exemplo/ifoodreplica/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.exemplo.ifoodreplica
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,16 +9,27 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.setupWithNavController
+import androidx.core.view.updatePadding
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navView: BottomNavigationView
+    lateinit var navHostView: FragmentContainerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        navHostView = findViewById(R.id.nav_host)
+        navView = findViewById(R.id.nav_view)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+            navHostView.updatePadding(left = systemBars.left, right = systemBars.right, top = systemBars.top)
+            navView.updatePadding(bottom = systemBars.bottom)
+            WindowInsetsCompat.CONSUMED
         }
 
         // Nota: É mais seguro pegar o NavController através do FragmentManager
@@ -25,7 +37,6 @@ class MainActivity : AppCompatActivity() {
 
         val navController = navHostFragment.navController
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.setupWithNavController(navController)
     }
 }
