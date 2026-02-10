cebc9d7612a87238f1572d1abfb52dbab38d53bc
dom_Ryan <ryanrodriguues06@gmail.com>
2026-02-09 21:25:28 -0300

    feat: Finalizando o projeto

--- app/src/main/java/com/exemplo/ifoodreplica/search/SearchFragment.kt
+++ app/src/main/java/com/exemplo/ifoodreplica/search/SearchFragment.kt
@@ -5,12 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.exemplo.ifoodreplica.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,16 +16,24 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var colLinearLayout: LinearLayout
+    private lateinit var colLinearLayout2: LinearLayout
+
+    // A estrutura da array dos items são ["nome do produto", "id da imagem: formato string"]
+    val  array_cards = arrayListOf(
+        arrayListOf("Pizza", "img_pizza_card"),
+        arrayListOf("Lanches", "img_burguer"),
+        arrayListOf("Japonesa", "img_sushi_card"),
+        arrayListOf("Brasileira", "img_brasil_card"),
+        arrayListOf("Crepes e Panquecas", "img_cp_card"),
+        arrayListOf("Bebidas", "img_bebida_card"),
+        arrayListOf("Hot Dogs", "img_hotdog_card"),
+        arrayListOf("Açaí", "img_acai_card")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -38,23 +44,43 @@ class SearchFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        colLinearLayout = view.findViewById<LinearLayout>(R.id.coluna1)
+        colLinearLayout2 = view.findViewById<LinearLayout>(R.id.coluna2)
+
+        val array_cards_col1 = array_cards.size / 2
+
+        for (i in 0 until array_cards.size) {
+            if (i % 2 == 0) {
+                // Se o número for PAR (0, 2, 4, 6) -> Coluna 1
+                makeCards(array_cards[i], colLinearLayout)
+            } else {
+                // Se o número for ÍMPAR (1, 3, 5, 7) -> Coluna 2
+                makeCards(array_cards[i], colLinearLayout2)
             }
+        }
+
     }
-}
+
+    fun makeCards(cardProduto: List<String>, coluna: LinearLayout){
+            val cardView = layoutInflater.inflate(R.layout.cards_view, null)
+
+            val texto = cardView.findViewById<TextView>(R.id.text_name_card)
+            val image = cardView.findViewById<ImageView>(R.id.image_view_card)
+
+            texto.text = cardProduto[0]
+            val imageName = cardProduto[1]
+
+            val resourceId = resources.getIdentifier(
+                imageName,
+                "drawable",
+                context?.packageName
+            )
+
+            image.setImageResource(resourceId)
+
+            coluna.addView(cardView)
+        }
+    }
